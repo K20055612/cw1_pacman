cd86a679c7cddd8fcc7edda8cdb6b82c85efa067
Nicolae-Marian Gartu <gartu.marian1973@gmail.com>
2023-02-21 15:48:12 +0000

    Refactor Split into class and add comments

--- classifier.py
+++ classifier.py
@@ -1,9 +1,8 @@
 # classifier.py
-# Lin Li/26-dec-2021
+# Junchi Ren, Nicolae-Marian Gartu, Xiaotian Yuan, Zhuoyuan Li/ February 2023
 #
-# Use the skeleton below for the classifier and insert your code here.
+# Decision Tree Classifier using gini index to calculate information gain
 from statistics import mode
-from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 
 class Classifier:
@@ -21,94 +20,97 @@ class Classifier:
         
 
 class Node:
+    # class to represent decision tree node and appropriate fields
     def __init__(self, feature_index=None, threshold=None, left=None, right=None, info_gain=None, value=None):
-        
-        # for decision node
         self.feature_index = feature_index
         self.threshold = threshold
         self.left = left
         self.right = right
         self.info_gain = info_gain
-        
-        # for leaf node
         self.value = value
+
+class Split:
+    # class to represent split data and appropriate fields
+    def __init__(self, feature_index=None, threshold=None, data_left=None, data_right=None, target_left=None, target_right=None, info_gain=None):
+        
+        self.feature_index = feature_index
+        self.threshold = threshold
+        self.data_left = data_left
+        self.data_right = data_right
+        self.target_left = target_left
+        self.target_right = target_right
+        self.info_gain = info_gain
 
 
 class DecisionTree:
-    def __init__(self, min_samples_split=2, max_depth=2):
-        # constructor
-        
-        # initialize the root of the tree 
-        self.root = None
-        
-        # stopping conditions
-        self.min_samples_split = min_samples_split
-        self.max_depth = max_depth
-    
-    # def fit(self, data, target):
-    #     return self.create_tree(np.array(data), np.array(target))
+    # class to represent Decision tree
+    def __init__(self):
 
-    def build_tree(self, data, target, curr_depth=0):
-        # recursive function to build the tree 
-        X = data
-        Y = target
-        num_samples, num_features = np.shape(X)
+        self.root = None
+    
+    def fit(self, data, target):
+        self.root = self.create_tree(np.array(data), np.array(target))
+    
+    def predict(self, samples):
+        # predict a set of samples
         
-        # split until stopping conditions are met
-        if num_samples>=self.min_samples_split and curr_depth<=self.max_depth:
-            # find the best split
-            best_split = self.get_best_split(data, target, num_samples, num_features)
-            # check if information gain is positive
-            if best_split["info_gain"]>0:
-                # recur left
-                left_subtree = self.build_tree(best_split["data_left"], best_split["target_left"], curr_depth+1)
-                # recur right
-                right_subtree =  self.build_tree(best_split["data_right"], best_split["target_right"], curr_depth+1)
-                # return decision node
-                return Node(best_split["feature_index"], best_split["threshold"], 
-                            left_subtree, right_subtree, best_split["info_gain"])
+        preditions = [self.predict_single_sample(sample, self.root) for sample in samples]
+        return preditions
+    
+    def predict_single_sample(self, sample, tree=None):
+
+        if tree.value!=None: return tree.value
+        feature_val = sample[tree.feature_index]
+        # choose between left subtree and right subtree according to the threashold
+        if feature_val<=tree.threshold:
+            return self.predict_single_sample(sample, tree.left)
+        else:
+            return self.predict_single_sample(sample, tree.right)
+    
+
+    def create_tree(self, features, targets, depth=0):
+        sample_count, feature_count = np.shape(features)
         
-        # compute leaf node
-        leaf_value = self.calculate_leaf_value(Y)
-        # return leaf node
+        best_split = self.get_best_split(features, targets, sample_count, feature_count)
+        # if the data was split and info gain is positive then recursively create the tree
+        if best_split != None and best_split.info_gain>0:
+            left_subtree = self.create_tree(best_split.data_left, best_split.target_left, depth+1)
+            right_subtree =  self.create_tree(best_split.data_right, best_split.target_right, depth+1)
+            
+            return Node(best_split.feature_index, best_split.threshold, 
+                        left_subtree, right_subtree, best_split.info_gain)
+        
+        # if we get here, either the data cannot be split anymore or we have reached the maximum depth of the tree
+        leaf_value = self.calculate_value_of_node(targets)
         return Node(value=leaf_value)
     
-    def get_best_split(self, data, target, num_samples, num_features):
-        #function to find the best split
+    def get_best_split(self, data, target, sample_count, feature_count):
         
-        # dictionary to store the best split
-        best_split = {}
         max_info_gain = -float("inf")
-        
-        # loop over all the features
-        for feature_index in range(num_features):
+
+        best_split = None
+        for feature_index in range(feature_count):
             feature_values = data[:, feature_index]
-            possible_thresholds = np.unique(feature_values)
-            # loop over all the feature values present in the data
-            for threshold in possible_thresholds:
-                # get current split
+            # get all possible thresholds
+            # in our case a feature could either be 0 or 1 so this is not entirely necessary, but in general it is
+            thresholds = np.unique(feature_values)
+            for threshold in thresholds:
+                # calculate the split for this threshold
                 data_left, target_left, data_right, target_right = self.split(data, target, feature_index, threshold)
-                # check if childs are not null
-                if len(data_left)>0 and len(data_right)>0:
-                    y, left_y, right_y = target, target_left, target_right
-                    # compute information gain
-                    curr_info_gain = self.information_gain(y, left_y, right_y, "gini")
-                    # update the best split if needed
-                    if curr_info_gain>max_info_gain:
-                        best_split["feature_index"] = feature_index
-                        best_split["threshold"] = threshold
-                        best_split["data_left"] = data_left
-                        best_split["data_right"] = data_right
-                        best_split["target_left"] = target_left
-                        best_split["target_right"] = target_right
-                        best_split["info_gain"] = curr_info_gain
-                        max_info_gain = curr_info_gain
+                # if the data could be split continue, otherwise None will be returned, hence plurality classification would
+                # occur on the parent node
+                if len(data_left) != 0 and len(data_right) != 0:
+                    info_gain = self.information_gain(target, target_left, target_right)
+                    # if this split results in a better info gain update it as best split
+                    if info_gain>max_info_gain:
+                        best_split = Split(feature_index, threshold, data_left, data_right, target_left, target_right, info_gain)
+                        max_info_gain = info_gain
                         
         # return best split
         return best_split
     
     def split(self, data, target, feature_index, threshold):
-        # function to split the data
+        # split the data based on feature index and threshold
         data_left, target_left, data_right, target_right = [], [] ,[] ,[]
         for sample_index in range(data.shape[0]):
             if data[sample_index][feature_index]<=threshold:
@@ -120,65 +122,29 @@ class DecisionTree:
 
         return np.array(data_left), np.array(target_left), np.array(data_right), np.array(target_right), 
     
-    def information_gain(self, parent, l_child, r_child, mode="gini"):
-        # function to compute information gain
+    def information_gain(self, parent, left_subtree_labels, right_subtree_labels):
         
-        weight_l = len(l_child) / len(parent)
-        weight_r = len(r_child) / len(parent)
-        if mode=="gini":
-            gain = self.gini_index(parent) - (weight_l*self.gini_index(l_child) + weight_r*self.gini_index(r_child))
+        left_subtree_weight = len(left_subtree_labels) / len(parent)
+        right_subtree_weight = len(right_subtree_labels) / len(parent)
+
+
+        # formula from slides for info gain
+        gain = self.calculate_gini_impurity(parent) - (left_subtree_weight*self.calculate_gini_impurity(left_subtree_labels) + right_subtree_weight*self.calculate_gini_impurity(right_subtree_labels))
         return gain
     
-    def gini_index(self, y):
-        # function to compute gini index
+    def calculate_gini_impurity(self, y):
         
-        class_labels = np.unique(y)
+        classes = np.unique(y)
         gini = 0
-        for cls in class_labels:
-            p_cls = len(y[y == cls]) / len(y)
-            gini += p_cls**2
+        # formula from slides for gini impurity
+        for class_label in classes:
+            class_ratio = np.count_nonzero(y == class_label) / len(y)
+            gini = gini +  class_ratio**2
+
         return 1 - gini
         
-    def calculate_leaf_value(self, Y):
-        # function to compute leaf node
-        
-        # Y = list(Y)
-        # return max(list(Y), key=list(Y).count)
+    def calculate_value_of_node(self, Y):
+        # get the predominant class using mode
         return mode(list(Y))
     
-    def print_tree(self, tree=None, indent=" "):
-        # function to print the tree
-        
-        if not tree:
-            tree = self.root
-
-        if tree.value is not None:
-            print(tree.value)
-
-        else:
-            print("X_"+str(tree.feature_index), "<=", tree.threshold, "?", tree.info_gain)
-            print("%sleft:" % (indent), end="")
-            self.print_tree(tree.left, indent + indent)
-            print("%sright:" % (indent), end="")
-            self.print_tree(tree.right, indent + indent)
     
-    def fit(self, data, target):
-        # function to train the tree
-        self.root = self.build_tree(np.array(data), np.array(target))
-    
-    def predict(self, X):
-        #function to predict new dataset 
-        
-        preditions = [self.make_prediction(x, self.root) for x in X]
-        return preditions
-    
-    def make_prediction(self, x, tree=None):
-        #function to predict a single data point
-
-        if tree.value!=None: return tree.value
-        feature_val = x[tree.feature_index]
-        if feature_val<=tree.threshold:
-            return self.make_prediction(x, tree.left)
-        else:
-            return self.make_prediction(x, tree.right)
-        
